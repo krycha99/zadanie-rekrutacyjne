9b818a59171b48f2a64fa31893f3905efb2f3dc5
Krystian Sudoł <krystian.19_9@wp.pl>
2023-08-18 18:52:43 +0200

    Sekcja intro + poprawki nawigacji

--- scripts/app.js
+++ scripts/app.js
@@ -8,3 +8,12 @@ const toggleSearch = (search, button) =>{
     })
  }
  toggleSearch('search-bar', 'search-btn')
+
+ // Mobile menu
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.nav__menu')
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+})
