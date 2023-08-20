efeabec0f5924f4851aab83231d9b48df4ad5e0b
Krystian Sudoł <krystian.19_9@wp.pl>
2023-08-20 22:44:09 +0200

    Instagram block + footer

--- scripts/app.js
+++ scripts/app.js
@@ -17,3 +17,50 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 })
+
+//masonry
+const elem = document.querySelector('.grid');
+        const msnry = new Masonry( elem, {
+        itemSelector: '.grid-item',
+        gutter: 30,
+        fitWidth: true,
+        columnWidth: 460
+        });
+
+//gallery button
+const gbtn = document.querySelector('.more__button');
+const gdisplay = document.querySelectorAll('.grid-item');
+let current = 9
+gbtn.addEventListener('click', function() {
+    for(let i = current; i<current+3; i++){
+        if(gdisplay[i])
+        {gdisplay[i].style.display = 'block';
+    }
+    }
+    current+= 3;
+
+    if(current>=gdisplay.length)
+    {
+        event.target.style.display='none';
+    }
+
+    const msnry = new Masonry( elem, {
+        itemSelector: '.grid-item',
+        gutter: 30,
+        fitWidth: true,
+        columnWidth: 460
+        });
+
+})
+
+//gallery popup
+document.querySelectorAll('.grid-item img').forEach(image => {
+    image.onclick = () =>{
+        document.querySelector('.gallery__container .popup-img').style.display = 'block';
+        document.querySelector('.popup-img img').src = image.getAttribute('src');
+    }
+});
+
+document.querySelector('.gallery__container .popup-img span').onclick = () =>{
+    document.querySelector('.popup-img').style.display = 'none';
+}
